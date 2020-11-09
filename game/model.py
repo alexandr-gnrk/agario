1af81d78ffb4dc68cfb1ad4548c8ca19bfa587a1
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2020-11-09 15:53:37 +0200

    Model was refactored and commented.

--- game/model.py
+++ game/model.py
@@ -3,14 +3,17 @@ from cell import Cell
 from player import Player
 
 class Model():
+    """Class that represents game state."""
+
     def __init__(self, player, world_size=1000):
-        # means that size of world is [-2000, 2000], [2000, -2000] 
+        # means that size of world is [-world_size, world_size] 
         self._world_size = world_size
         self._player = player
         # self._players = players.append(player)
         self._cells = list()
 
     def move(self, angle, speed):
+        """Make player movement.""" 
         self.player.move(angle, speed)
         for cell in self._cells:
             if Player.is_collided(self._player, cell):
@@ -18,6 +21,7 @@ class Model():
                 self._cells.remove(cell)
 
     def spawn_cells(self, amount):
+        """Spawn passed amount of cells on the field."""
         for _ in range(amount):
             self._cells.append(Cell.make_random(self.world_size))
 
